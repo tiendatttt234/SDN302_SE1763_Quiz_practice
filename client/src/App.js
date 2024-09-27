c339d1a060b90548f07d4b598855f62ca5955d04
tiendatttt234 <nguyentiendat23402@gmail.com>
2024-09-27 23:02:23 +0700

    fe header, footer, hom page, login ,register

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/common/layout/Header";
+import HomePage from "./components/authen/Homepage";
+import Footer from "./components/common/layout/Footer";
+import Login from "./components/authen/Login";
+import Register from "./components/authen/Register";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
