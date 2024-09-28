de3ea93d4db34ebddfdddec4c9f0b0addeaaa3ac
annthe170622 <annthe170622@fpt.edu.vn>
2024-09-28 17:00:54 +0700

    fix bug1

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import PrivateRoute from './components/core/PrivateRoute';
 import UserDefaultPage from './components/common/layout/user-default-layout';
 import NoAccessPage from './components/common/pages/NoAccessPage';
+import HomePage from './components/authen/Homepage';
+import Login from './components/authen/Login/Login';
+import Register from './components/authen/Register/Register';
 
 function App() {
   return (
@@ -16,11 +19,10 @@ function App() {
           <Route path='/trainer/*' element={<PrivateRoute element={<DefaultLayoutTrainer />} allowedRoles={['trainer']} />} />
           <Route path={`${link.trainee}/*`} element={<PrivateRoute element={<DefaultLayoutTrainee />} allowedRoles={['trainee']} />} />
           */}
-          <Route path='/' element={<div>h3h3</div>}/>
           <Route path='/user/*' element={<UserDefaultPage/>} />
           {/* <PrivateRoute element={<DefaultLayoutAdmin />} allowedRoles={['admin']} /> */}
           <Route path="/no-access" element={<NoAccessPage />} />
-            <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
       </Routes>
