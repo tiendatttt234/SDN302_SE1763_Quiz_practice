611b3a7544ecaee86544196a6935a380018e2d25
annthe170622 <annthe170622@fpt.edu.vn>
2024-09-30 20:31:21 +0700

    Update manager-default-layout.jsx

--- client/src/components/common/layout/manager-default-layout.jsx
+++ client/src/components/common/layout/manager-default-layout.jsx
@@ -3,6 +3,7 @@ import ManagerDashboard from "../../manager/dashboard/dashboard";
 import AddQuestion from "../../manager/question/question";
 import ViewQuestionDetail from "../../manager/question/viewQuesDetail";
 import ManageQuestion from "../../manager/question/viewQues";
+import UpdateQuestion from "../../manager/question/updateQues";
 export default function ManagerDefaultPage() {
   return (
     <div>
@@ -12,6 +13,7 @@ export default function ManagerDefaultPage() {
         <Route path="/viewques" element={<ViewQuestionDetail />} />
         <Route path="/viewques/:id" element={<ViewQuestionDetail />}/>
         <Route path="/manaques" element={<ManageQuestion />} />
+        <Route path='/updatequestion/:id' element={<UpdateQuestion/>} />
       </Routes>
     </div>
   );
