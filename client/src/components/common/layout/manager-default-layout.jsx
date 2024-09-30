7e610512f928cf7be464fbc43c31d9d265f2c5ed
annthe170622 <annthe170622@fpt.edu.vn>
2024-09-30 20:56:26 +0700

    merger all

--- client/src/components/common/layout/manager-default-layout.jsx
+++ client/src/components/common/layout/manager-default-layout.jsx
@@ -4,6 +4,9 @@ import AddQuestion from "../../manager/question/question";
 import ViewQuestionDetail from "../../manager/question/viewQuesDetail";
 import ManageQuestion from "../../manager/question/viewQues";
 import UpdateQuestion from "../../manager/question/updateQues";
+import ManageBlog from "../../manager/blog/manageBlog";
+import ManageCreateBlog from "../../manager/blog/createBlog";
+import ManageEditBlog from "../../manager/blog/editBlog";
 export default function ManagerDefaultPage() {
   return (
     <div>
@@ -14,6 +17,12 @@ export default function ManagerDefaultPage() {
         <Route path="/viewques/:id" element={<ViewQuestionDetail />}/>
         <Route path="/manaques" element={<ManageQuestion />} />
         <Route path='/updatequestion/:id' element={<UpdateQuestion/>} />
+        <Route path="/manageBlog/" element={<ManageBlog />} />
+        <Route path="/manageBlog/add-blog" element={<ManageCreateBlog />} />
+        <Route
+            path="/manageBlog/edit-blog/:id"
+            element={<ManageEditBlog />}
+          />
       </Routes>
     </div>
   );
