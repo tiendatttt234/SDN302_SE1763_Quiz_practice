41487ba92fbf1e5cf244a75444416ee283898d56
annthe170622 <annthe170622@fpt.edu.vn>
2024-10-21 22:36:11 +0700

    setup api
    .

--- api/models/Role.model.js
+++ api/models/Role.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const RoleSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    }
 })
 
 const Role = mongoose.model("Role", RoleSchema);
