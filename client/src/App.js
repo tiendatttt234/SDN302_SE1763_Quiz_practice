019dff359aa44c48caecc446870ec061ca6e62b9
annthe170622 <annthe170622@fpt.edu.vn>
2024-10-27 21:36:18 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,63 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import PrivateRoute from './components/core/PrivateRoute';
-import Header from "./components/common/layout/Header/Header";
-import Footer from "./components/common/layout/Footer/Footer";
-import UserDefaultPage from './components/common/layout/user-default-layout';
-import NoAccessPage from './components/common/pages/NoAccessPage';
+import PrivateRoute from "./components/core/PrivateRoute";
+import HomePage from "./components/authen/Homepage";
+import Login from "./components/authen/Login";
+import Register from "./components/authen/Register";
+import Header from "./components/common/layout/Header";
+import MyCourse from "./components/authen/MyCourse";
+import NoAccessPage from "./components/common/pages/NoAccessPage";
 import ManagerDefaultPage from "./components/common/layout/manager-default-layout";
-import UpgradePage from "./components/common/pages/UpgradePage";
-import CheckoutForm from "./components/common/pages/CheckOutPage";
-import BlogList from "./components/user/BlogList";
-import BlogDetail from "./components/user/BlogDetail";
-import AuthRoutes from "./components/authen/AuthRoute";
-import AdminDefaultPage from "./components/common/layout/admin-default-layout";
-import ImportFilePage from "./components/admin/adminComponnents/importQuestion";
-
+import AdminProfit from "./components/admin/viewProfit";
+import AdminDashboard from "../src/components/admin/adminComponnents/admin";
+import UserManagement from "./components/admin/adminComponnents/usermanagement";
+import Footer from "./components/common/layout/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Header/>
-      <Routes>
-          {/* <Route path='/*' element={<AuthRoutes />} />
-          <Route path='*' element={<PageNotFound />} />
-          <Route path='/manager/*' element={<PrivateRoute element={<DefaultLayoutManager />} allowedRoles={['manager']} />} />
-          
-          <Route path='/trainer/*' element={<PrivateRoute element={<DefaultLayoutTrainer />} allowedRoles={['trainer']} />} />
-          <Route path={`${link.trainee}/*`} element={<PrivateRoute element={<DefaultLayoutTrainee />} allowedRoles={['trainee']} />} />
-          */}
-          <Route path="/*" element={<AuthRoutes />} />
-          <Route path="/user/*" element={<UserDefaultPage />} />
-          <Route path="/managerdb/*" element={<ManagerDefaultPage/>}/> {/* bỏ role manager   */}
-          <Route path="admin/*" element={<AdminDefaultPage/>}/>
-          
-
-
-
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/mycourse" element={<MyCourse />} />
           <Route path="/no-access" element={<NoAccessPage />} />
-          <Route path="/upgrade" element={<UpgradePage/>}/>
-          <Route path="/checkout" element={<CheckoutForm />} />
-          <Route path="/blogList" element={<BlogList/>}/>
-          <Route path="/blogDetail" element={<BlogDetail/>}/>
 
-          <Route path="/addQuestion" element={<ImportFilePage/>}/>
+          {/* Admin Routes */}
+          <Route
+            path="/admin/*"
+            element={
+              <PrivateRoute element={<AdminDashboard />} requiredRole="admin" />
+            }
+          />
+          <Route
+            path="/admin/profit"
+            element={
+              <PrivateRoute element={<AdminProfit />} requiredRole="admin" />
+            }
+          />
+          <Route
+            path="/admin/users"
+            element={
+              <PrivateRoute element={<UserManagement />} requiredRole="admin" />
+            }
+          />
 
-      
-      </Routes>
-      <Footer />
-    </div>
+          {/* Manager Routes */}
+          <Route
+            path="/managerdb/*"
+            element={
+              <PrivateRoute
+                element={<ManagerDefaultPage />}
+                requiredRole="manager"
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
