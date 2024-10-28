fe7b50415adaf699d57799bb123a8eafa4ae038f
annthe170622 <annthe170622@fpt.edu.vn>
2024-10-28 08:26:39 +0700

    fix router app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,44 @@
 import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/core/PrivateRoute";
 import HomePage from "./components/authen/Homepage";
-import MyCourse from "./components/authen/MyCourse";
 import NoAccessPage from "./components/common/pages/NoAccessPage";
-import ManagerDefaultPage from "./components/common/layout/manager-default-layout";
-import AdminProfit from "./components/admin/viewProfit";
-import AdminDashboard from "../src/components/admin/adminComponnents/admin";
-import UserManagement from "./components/admin/adminComponnents/usermanagement";
+import AdminDefaultPage from "./components/common/layout/admin-default-layout";
 import Header from "./components/common/layout/Header/Header";
 import Login from "./components/authen/Login/Login";
 import Register from "./components/authen/Register/Register";
 import Footer from "./components/common/layout/Footer/Footer";
+import UserDefaultPage from "./components/common/layout/user-default-layout";
+import FlashcardPage from "./components/user/FlashCard";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
+          {/* Common Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/mycourse" element={<MyCourse />} />
           <Route path="/no-access" element={<NoAccessPage />} />
+          <Route path='/flash' element={FlashcardPage}/>
+
+
+
+          {/* User Routes  */}
+          <Route
+            path="/admin/*"
+            element={
+              <PrivateRoute element={<UserDefaultPage />} requiredRole="user" />
+            }
+          />
 
           {/* Admin Routes */}
           <Route
             path="/admin/*"
             element={
-              <PrivateRoute element={<AdminDashboard />} requiredRole="admin" />
-            }
-          />
-          <Route
-            path="/admin/profit"
-            element={
-              <PrivateRoute element={<AdminProfit />} requiredRole="admin" />
-            }
-          />
-          <Route
-            path="/admin/users"
-            element={
-              <PrivateRoute element={<UserManagement />} requiredRole="admin" />
-            }
-          />
-
-          {/* Manager Routes */}
-          <Route
-            path="/managerdb/*"
-            element={
-              <PrivateRoute
-                element={<ManagerDefaultPage />}
-                requiredRole="manager"
-              />
+              <PrivateRoute element={<AdminDefaultPage />} requiredRole="admin" />
             }
           />
         </Routes>
