f4ffecae2dd04454dd1c954126e9e4fa9534a0a9
annthe170622 <annthe170622@fpt.edu.vn>
2024-10-28 10:44:09 +0700

    fix conflict 1

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,6 @@ const QuestionFileRouter = require("./questionFile.router");
 const QuizRouter = require("./quiz.router");
 const QuizSubmitRouter = require("./quizSubmit.router");
 const AccountRouter = require("./account.router");
-const QuestionFileRouter = require('./questionFile.router');
 module.exports = {
   QuizRouter,
   QuestionFileRouter,
